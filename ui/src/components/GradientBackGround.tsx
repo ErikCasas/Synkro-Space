24f7b4eeba83cf0328bf30f2ad520ce0b54201e8
Erik Casas <ehernandez@heimcore.com.co>
2025-11-09 23:58:52 -0500

    ui fix: made background fixed

--- ui/src/components/GradientBackGround.tsx
+++ ui/src/components/GradientBackGround.tsx
@@ -25,31 +25,41 @@ export const GradientBackground = () => {
   }, [])
 
   return (
-    <div className="gradient-bg relative w-screen h-screen overflow-hidden">
+    <>
+  <div
+      className="
+        fixed inset-0 
+        -z-10 
+        pointer-events-none
+        overflow-hidden
+      "
+    >
+      <div className="gradient-bg w-full h-full">
+        <svg className="fixed top-0 left-0 w-0 h-0" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
 
-      <svg className="fixed top-0 left-0 w-0 h-0" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <filter id="goo">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-            <feColorMatrix
-              in="blur"
-              mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-              result="goo"
-            />
-            <feBlend in="SourceGraphic" in2="goo" />
-          </filter>
-        </defs>
-      </svg>
-
-      <div className="gradients-container w-full h-full">
-        <div className="g g1"></div>
-        <div className="g g2"></div>
-        <div className="g g3"></div>
-        <div className="g g4"></div>
-        <div className="g g5"></div>
-        <div className="interactive"></div>
+        <div className="gradients-container w-full h-full">
+          <div className="g g1"></div>
+          <div className="g g2"></div>
+          <div className="g g3"></div>
+          <div className="g g4"></div>
+          <div className="g g5"></div>
+          <div className="interactive"></div>
+        </div>
       </div>
     </div>
+    </>
   )
 }
