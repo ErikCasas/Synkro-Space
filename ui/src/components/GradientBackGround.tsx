0b1b165d79ab731b169fdfcb0dece1626da88168
Erik Casas <ehernandez@heimcore.com.co>
2025-11-10 09:38:50 -0500

    ui chore: use ref hook for gradients

--- ui/src/components/GradientBackGround.tsx
+++ ui/src/components/GradientBackGround.tsx
@@ -1,18 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import './GradientBackGround.css'
 
 export const GradientBackground = () => {
-  useEffect(() => {
-    const interBubble = document.querySelector<HTMLDivElement>(".interactive")
-    if (!interBubble) return
 
+  const interBubble = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
     let curX = 0, curY = 0, tgX = 0, tgY = 0
 
     const move = () => {
       curX += (tgX - curX) / 20
       curY += (tgY - curY) / 20
-      interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`
-      requestAnimationFrame(move)
+
+      if (interBubble.current) {
+        interBubble.current.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`
+        requestAnimationFrame(move)
+      }
     }
 
     window.addEventListener("mousemove", (e) => {
@@ -20,46 +23,46 @@ export const GradientBackground = () => {
       tgY = e.clientY
     })
 
-    move()
-    return () => window.removeEventListener("mousemove", () => {})
+    move()  
+    return () => window.removeEventListener("mousemove", () => { })
   }, [])
 
   return (
     <>
-  <div
-      className="
+      <div
+        className="
         fixed inset-0 
         -z-10 
         pointer-events-none
         overflow-hidden
       "
-    >
-      <div className="gradient-bg w-full h-full">
-        <svg className="fixed top-0 left-0 w-0 h-0" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <filter id="goo">
-              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-              <feColorMatrix
-                in="blur"
-                mode="matrix"
-                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                result="goo"
-              />
-              <feBlend in="SourceGraphic" in2="goo" />
-            </filter>
-          </defs>
-        </svg>
+      >
+        <div className="gradient-bg w-full h-full">
+          <svg className="fixed top-0 left-0 w-0 h-0" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                <feColorMatrix
+                  in="blur"
+                  mode="matrix"
+                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                  result="goo"
+                />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
 
-        <div className="gradients-container w-full h-full">
-          <div className="g g1"></div>
-          <div className="g g2"></div>
-          <div className="g g3"></div>
-          <div className="g g4"></div>
-          <div className="g g5"></div>
-          <div className="interactive"></div>
+          <div className="gradients-container w-full h-full">
+            <div className="g g1"></div>
+            <div className="g g2"></div>
+            <div className="g g3"></div>
+            <div className="g g4"></div>
+            <div className="g g5"></div>
+            <div className="interactive" ref={interBubble}></div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
